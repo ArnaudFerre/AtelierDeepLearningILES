3b33f195390ff364be7598af0efba10a380418a7
Julien Tourille <julien@latribu.org>
2017-07-03 13:56:33 +0200

    Add remove labels launcher option

--- workshops/workshop_01/launcher_w01pkg.py
+++ workshops/workshop_01/launcher_w01pkg.py
@@ -2,7 +2,7 @@ import argparse
 import logging
 import os
 
-from ncbitools.conll import convert_to_conll
+from w01pkg.conll import convert_to_conll, remove_label
 
 if __name__ == "__main__":
 
@@ -20,6 +20,13 @@ if __name__ == "__main__":
     parser_conll.add_argument("--corenlp_url", help="CoreNLP server url", dest="corenlp_url", type=str,
                               required=True)
 
+    # Remove labels
+    parser_rm_labels = subparsers.add_parser('REMOVE-LABELS', help="Remove token labels from tabulated file")
+    parser_rm_labels.add_argument("--input_file", help="Input tabulated file", dest="input_file", type=str,
+                                  required=True)
+    parser_rm_labels.add_argument("--output_file", help="Output tabulated file", dest="output_file", type=str,
+                                  required=True)
+
     args = parser.parse_args()
 
     if args.subparser_name == "CONVERT":
@@ -31,3 +38,15 @@ if __name__ == "__main__":
         logging.info("* CoreNLP server address: {}".format(args.corenlp_url))
 
         convert_to_conll(args.input_file, args.output_file, args.corenlp_url)
+
+    elif args.subparser_name == "REMOVE-LABELS":
+
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+        if os.path.isfile(os.path.abspath(args.output_file)):
+            raise FileExistsError("The output file already exists")
+
+        logging.info("Converting file: {}".format(os.path.abspath(args.input_file)))
+        logging.info("* Target file: {}".format(os.path.abspath(args.output_file)))
+
+        remove_label(args.input_file, args.output_file)
