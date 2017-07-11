ae9610bf6aef73d32133a6000636790c6eef7ddf
Julien Tourille <julien@latribu.org>
2017-07-11 23:57:42 +0200

    Add import and correct call to function

--- workshops/workshop_01/w01pkg/ncbi.py
+++ workshops/workshop_01/w01pkg/ncbi.py
@@ -3,6 +3,8 @@ import re
 
 import gensim
 
+from .tools import load_dataset
+
 ncbi_mapping = ["B-Disease", "I-Disease", "O"]
 
 
@@ -15,50 +17,11 @@ def load_ncbi(train_file, dev_file, test_file, gensim_model_path):
     for i, key in enumerate(gensim_model.wv.index2word):
         word_dict[key] = i
 
-    x_train, y_train = _load_dataset(train_file, word_dict, word_set)
-    x_dev, y_dev = _load_dataset(dev_file, word_dict, word_set)
-    x_test, y_test = _load_dataset(test_file, word_dict, word_set)
+    x_train, y_train = load_dataset(train_file, word_dict, word_set, ncbi_mapping)
+    x_dev, y_dev = load_dataset(dev_file, word_dict, word_set, ncbi_mapping)
+    x_test, y_test = load_dataset(test_file, word_dict, word_set, ncbi_mapping)
 
     return (x_train, y_train), (x_dev, y_dev), (x_test, y_test)
-
-
-def _load_dataset(data_file, word_dict, word_set):
-
-    x_data = list()
-    y_data = list()
-
-    current_sequence = list()
-    current_labels = list()
-
-    with open(os.path.abspath(data_file), "r", encoding="UTF-8") as input_file:
-
-        for i, line in enumerate(input_file):
-
-            if re.match("^$", line):
-                if len(current_sequence) > 0:
-                    x_data.append(current_sequence)
-                    y_data.append(current_labels)
-
-                    current_sequence = list()
-                    current_labels = list()
-                continue
-
-            parts = line.rstrip("\n").split("\t")
-
-            token_str = re.sub("\d", "0", parts[0].lower())
-
-            if token_str in word_set:
-                current_sequence.append(word_dict[token_str])
-            else:
-                current_sequence.append(word_dict["#unk#"])
-
-            current_labels.append(ncbi_mapping.index(parts[-1]))
-
-    if len(current_sequence) > 0:
-        x_data.append(current_sequence)
-        y_data.append(current_labels)
-
-    return x_data, y_data
 
 
 def generate_output(original_test_file, output_test_file, y_pred):
