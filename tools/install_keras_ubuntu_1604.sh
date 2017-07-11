edc14edbfa9085ac5b8889abe7c3e68adc687b10
Vincent Letard <vincent.letard@limsi.fr>
2017-07-11 07:39:34 +0200

    Proper keras installation on ubuntu 16.04 with python3

---
+++ tools/install_keras_ubuntu_1604.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install python3-setuptools python3-numpy python3-scipy python3-dev
+
+cd /tmp/
+
+wget "https://github.com/fchollet/keras/tarball/2.0.6" -O - 2> /dev/null | tar xvz
+
+cd fchollet-keras-f120a56/
+
+sudo python3 ./setup.py install
+
+cat > ~/.keras/keras.json << EOF
+{
+    "image_data_format": "channels_last",
+    "epsilon": 1e-07,
+    "floatx": "float32",
+    "backend": "theano"
+}
